88f8409a127dc164a4a05f5896619080f8ce0c03
Bipin Ghimire <bipinghimire2072@gmail.com>
2023-05-12 15:27:42 +0545

    Final Commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -61,26 +61,30 @@ const App = () => {
 
   return (
     <>
-      <Navbar />
+      <Navbar className="navv"/>
       <main>
+        <div className="bodysection">
+          <form>
+            <h1 className="title">Add Notes</h1>  
+            <div className="mb-3">
+                <label for="title" className="form-label"><b>Note Title</b></label>
+                <input type="text" className="form-control form" value={titleInput} onChange={e=>setTitleInput(e.target.value)} id="exampleInputEmail1" placeholder='Enter the title of your note' />
+            </div>
 
-      
-        <h1 className="title">Note Manager</h1>
-        <form>
-          <div className="mb-3">
-              <label for="title" className="form-label">Note Title</label>
-              <input type="text" className="form-control" value={titleInput} onChange={e=>setTitleInput(e.target.value)} id="exampleInputEmail1" placeholder='Enter the title of your note' />
+            <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label"><b>Description</b></label>
+                <textarea class="form-control form" value={input} onChange={e=>setInput(e.target.value)} id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+            <button type="submit" className="btn btn-primary" onClick={updateId ? updateTask : addTask}>{updateId ? "Update Note" : "Add Note"}</button>
+          
+          </form>
+          <div className="listpart">
+            <h1>Your Notes</h1>
+            <ul>
+              {tasks.map((task,title, date) => <List key={task._id} date={task.date} id={task._id} task={task.task} title={task.title} setUpdateUI={setUpdateUI} updateMode={updateMode}/>)}
+            </ul>
           </div>
-
-          <div class="mb-3">
-              <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-              <textarea class="form-control" value={input} onChange={e=>setInput(e.target.value)} id="exampleFormControlTextarea1" rows="3"></textarea>
-          </div>
-          <button type="submit" className="btn btn-primary" onClick={updateId ? updateTask : addTask}>{updateId ? "Update Task" : "Add Task"}</button>
-        </form>
-        <ul>
-          {tasks.map((task,title, date) => <List key={task._id} date={task.date} id={task._id} task={task.task} title={task.title} setUpdateUI={setUpdateUI} updateMode={updateMode}/>)}
-        </ul>
+        </div>
       </main>
     </>
   )
